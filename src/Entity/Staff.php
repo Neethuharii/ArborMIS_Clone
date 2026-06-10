133ebc940ebbb514091cc62d995d1ea85c6a3fd6
Neethu <Neethu@gmail.com>
2026-06-10 12:29:57 +0530

    create staff table for login authentication

---
+++ src/Entity/Staff.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StaffRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: StaffRepository::class)]
+class Staff
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $StaffEmail = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $password = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getStaffEmail(): ?string
+    {
+        return $this->StaffEmail;
+    }
+
+    public function setStaffEmail(string $StaffEmail): static
+    {
+        $this->StaffEmail = $StaffEmail;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+}
