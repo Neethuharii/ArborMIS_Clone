7bb7a10224d46e298eea4338ed33d76882495e1c
Neethu <Neethu@gmail.com>
2026-06-10 14:05:17 +0530

    Removed all auto-generated comments from migration files

--- migrations/Version20260610062002.php
+++ migrations/Version20260610062002.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20260610062002 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,13 +17,13 @@ final class Version20260610062002 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+       
         $this->addSql('CREATE TABLE staff (id INT AUTO_INCREMENT NOT NULL, staff_email VARCHAR(255) NOT NULL, password VARCHAR(150) NOT NULL, PRIMARY KEY (id)) DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+       
         $this->addSql('DROP TABLE staff');
     }
 }
