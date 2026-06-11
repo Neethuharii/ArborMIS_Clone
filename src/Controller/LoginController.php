d0ce4ba42c7f819009031b47538ef789f0b9739f
Neethu <Neethu@gmail.com>
2026-06-11 13:03:52 +0530

    format all files and improve code structure

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,15 +7,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
-
 final class LoginController extends AbstractController
 {
-
     #[Route(path: '/login', name: 'login')]
     public function login(): Response
     {
-
         return $this->render('login/index.html.twig');
     }
 }
