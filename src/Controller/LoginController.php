ed46f1829058ea407a742586cb84269f145fa779
Neethu <Neethu@gmail.com>
2026-06-11 10:58:57 +0530

    modified login page design and added logo

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,21 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
 
 final class LoginController extends AbstractController
 {
 
-     #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/login', name: 'login')]
     public function login(): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('home_page'); //it will redirect to home page after successfull login
-        }
 
         return $this->render('login/index.html.twig');
     }
