0942188c8fe3152350d61eca37983a900742ebb8
jenitta2 <jenittasupriya@gmail.com>
2026-06-11 12:48:58 +0530

    Made the space changes and alignment issues

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -5,19 +5,14 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomepageController extends AbstractController
+final class HomepageController extends AbstractController
 {
-
     #[Route('/Homepage/home', name: 'homepage')]
-
     public function homepage(): Response
     {
-
         return $this->render('/Homepage/home.html.twig');
     }
 }
