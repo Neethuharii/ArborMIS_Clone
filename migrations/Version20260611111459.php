118caccbbf6dc2e9b76db65d4f68c4b02efd4ec8
Neethu <Neethu@gmail.com>
2026-06-11 16:54:27 +0530

    Generated a migration version file for table religion

--- migrations/Version20260611111459.php
+++ migrations/Version20260611111459.php
@@ -8,24 +8,23 @@ use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 final class Version20260611111459 extends AbstractMigration
 {
-    public function getDescription(): string
+     public function getDescription(): string
     {
-        return 'Create religions master table with religion_id and gender_type columns';
+        return 'Create religions master table with religion_id and religion_name columns';
     }
 
     public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE genders');
-        $this->addSql('ALTER TABLE religions MODIFY religion_id INT NOT NULL');
-        $this->addSql('ALTER TABLE religions CHANGE religion_id id INT AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
+    {  
+        $this->addSql('
+            CREATE TABLE IF NOT EXISTS religions
+                (religion_id INT AUTO_INCREMENT NOT NULL, 
+                religion_name VARCHAR(255) NOT NULL,
+                PRIMARY KEY (religion_id))
+                DEFAULT CHARACTER SET utf8mb4');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE genders (gender_id INT AUTO_INCREMENT NOT NULL, gender_type VARCHAR(150) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_0900_ai_ci`, PRIMARY KEY (gender_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_0900_ai_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE religions MODIFY id INT NOT NULL');
-        $this->addSql('ALTER TABLE religions CHANGE id religion_id INT AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (religion_id)');
+        $this->addSql('DROP TABLE IF EXISTS religions');
     }
 }
