73dba6cb36eaef2b04cffc17e1e34062e740b683
jenitta2 <jenittasupriya@gmail.com>
2026-06-14 21:29:51 +0530

    Created a master table for address

---
+++ src/Entity/Address.php
@@ -0,0 +1,187 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\AddressRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AddressRepository::class)]
+class Address
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $address_id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $address1 = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $address2 = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $address3 = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $city = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $postCode = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $county = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $emailAddress = null;
+
+    #[ORM\Column]
+    private ?int $phoneNumber = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $modified_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $deleted_at = null;
+
+    public function getAddressId(): ?int
+    {
+        return $this->address_id;
+    }
+
+    public function getAddress1(): ?string
+    {
+        return $this->address1;
+    }
+
+    public function setAddress1(string $address1): static
+    {
+        $this->address1 = $address1;
+
+        return $this;
+    }
+
+    public function getAddress2(): ?string
+    {
+        return $this->address2;
+    }
+
+    public function setAddress2(?string $address2): static
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    public function getAddress3(): ?string
+    {
+        return $this->address3;
+    }
+
+    public function setAddress3(?string $address3): static
+    {
+        $this->address3 = $address3;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): static
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPostCode(): ?string
+    {
+        return $this->postCode;
+    }
+
+    public function setPostCode(string $postCode): static
+    {
+        $this->postCode = $postCode;
+
+        return $this;
+    }
+
+    public function getCounty(): ?string
+    {
+        return $this->county;
+    }
+
+    public function setCounty(string $county): static
+    {
+        $this->county = $county;
+
+        return $this;
+    }
+
+    public function getEmailAddress(): ?string
+    {
+        return $this->emailAddress;
+    }
+
+    public function setEmailAddress(string $emailAddress): static
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    public function getPhoneNumber(): ?int
+    {
+        return $this->phoneNumber;
+    }
+
+    public function setPhoneNumber(int $phoneNumber): static
+    {
+        $this->phoneNumber = $phoneNumber;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeImmutable
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(\DateTimeImmutable $modified_at): static
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(\DateTimeImmutable $deleted_at): static
+    {
+        $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+}
