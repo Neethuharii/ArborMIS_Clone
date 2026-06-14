f723e56e9d0bfa2abc4ccecfbcb301b9ab7a6189
Neethu <Neethu@gmail.com>
2026-06-14 16:14:13 +0530

    Created a card table for storing school id card details

---
+++ migrations/Version20260614103952.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260614103952 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Created a card table for storing school id card details';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE IF NOT EXISTS cards (
+                  card_id INT AUTO_INCREMENT NOT NULL,
+                  card_number VARCHAR(150) NOT NULL, 
+                  status VARCHAR(100) NOT NULL,
+                  issued_at DATETIME NOT NULL, 
+                  modified_at DATETIME NOT NULL, 
+                  deleted_at DATETIME DEFAULT NULL, 
+                  PRIMARY KEY (card_id)) 
+                  DEFAULT CHARACTER SET utf8mb4');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE IF EXISTS cards');
+    }
+}
