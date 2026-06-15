cc848931e42cf6553645c5378254656fbbbb06e9
jenitta2 <jenittasupriya@gmail.com>
2026-06-15 12:02:20 +0530

    Created a master table for nationality status

---
+++ migrations/Version20260615062502.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+
+final class Version20260615062502 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create tables for nationalities';
+    }
+
+    public function up(Schema $schema): void
+    {
+         $this->addSql('CREATE TABLE nationality 
+         (nationality_id INT AUTO_INCREMENT NOT NULL, 
+         nationality_status VARCHAR(100) NOT NULL, 
+         PRIMARY KEY (nationality_id)) 
+         DEFAULT CHARACTER SET utf8');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE IF EXISTS nationality');
+    }
+}
