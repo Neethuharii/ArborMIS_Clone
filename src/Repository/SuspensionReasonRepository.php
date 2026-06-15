797fd06dcb23127265a7a25d7f17ed3c8f3a8ec3
Mohammed Sahal <mohammed.sahal@integris-education.com>
2026-06-15 09:59:25 +0530

    Removed line break from suspension repository file

--- src/Repository/SuspensionReasonRepository.php
+++ src/Repository/SuspensionReasonRepository.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
-
 use App\Entity\SuspensionReasons;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
