7efe5bfda283de5f8bf3ac05a0effc1989437f15
jenitta2 <jenittasupriya@gmail.com>
2026-06-15 17:40:42 +0530

    Removing files for renaming the table

--- src/Entity/BusinessRoles.php
+++ src/Entity/BusinessRoles.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 namespace App\Entity;
 
@@ -6,7 +7,7 @@ use App\Repository\BusinessRoleRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: BusinessRoleRepository::class)]
-class BusinessRole
+class BusinessRoles
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
