12b57c1f2f93ef21fa9975f1fef73a313c19d3db
jenitta2 <jenittasupriya@gmail.com>
2026-06-15 15:49:19 +0530

    Created a table to store qualification checks

---
+++ src/Entity/QualificationChecks.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\QualificationChecksRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: QualificationChecksRepository::class)]
+class QualificationChecks
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(targetEntity: QualificationType::class, inversedBy: 'qualificationChecks')]
+    #[ORM\JoinColumn(name: 'qualification_id', referencedColumnName: 'qualification_id', nullable: false)]
+    private ?QualificationType $qualificationType = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $clearance_level = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $requested_date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $returned_date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTime $authenticated_date = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $modified_at = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $deleted_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getQualificationType(): ?QualificationType
+    {
+        return $this->qualificationType;
+    }
+
+    public function setQualificationType(?QualificationType $qualificationType): static
+    {
+        $this->qualificationType = $qualificationType;
+        return $this;
+    }
+
+    public function getClearanceLevel(): ?string
+    {
+        return $this->clearance_level;
+    }
+
+    public function setClearanceLevel(string $clearance_level): static
+    {
+        $this->clearance_level = $clearance_level;
+
+        return $this;
+    }
+
+    public function getRequestedDate(): ?\DateTime
+    {
+        return $this->requested_date;
+    }
+
+    public function setRequestedDate(\DateTime $requested_date): static
+    {
+        $this->requested_date = $requested_date;
+
+        return $this;
+    }
+
+    public function getReturnedDate(): ?\DateTime
+    {
+        return $this->returned_date;
+    }
+
+    public function setReturnedDate(\DateTime $returned_date): static
+    {
+        $this->returned_date = $returned_date;
+
+        return $this;
+    }
+
+    public function getAuthenticatedDate(): ?\DateTime
+    {
+        return $this->authenticated_date;
+    }
+
+    public function setAuthenticatedDate(\DateTime $authenticated_date): static
+    {
+        $this->authenticated_date = $authenticated_date;
+
+        return $this;
+    }
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(?string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeImmutable
+    {
+        return $this->modified_at;
+    }
+
+    public function setModifiedAt(\DateTimeImmutable $modified_at): static
+    {
+        $this->modified_at = $modified_at;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(?\DateTimeImmutable $deleted_at): static
+    {
+        $this->deleted_at = $deleted_at;
+
+        return $this;
+    }
+}
