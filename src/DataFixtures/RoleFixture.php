f24d193c96a81a809ff020d73fb7245df827f29d
jenitta2 <jenittasupriya@gmail.com>
2026-06-17 14:55:36 +0530

    Formatted the file

--- src/DataFixtures/RoleFixture.php
+++ src/DataFixtures/RoleFixture.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\BusinessRoles;
@@ -10,14 +12,22 @@ class RoleFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $values = ['Administration Assistant','Admission Officer','Advisory Teacher','Art/Design Technician','Assistant Head Teacher','Attendance Officer','Behaviour Manager/Specialist','Bursar/Business Manager','Careers Advisor','Child Protection Liaison Officer','Childcare Officer'];
+        $values = [
+            'Administration Assistant','Admission Officer','Advisory Teacher','Art/Design Technician',
+            'Assistant Head Teacher','Attendance Officer','Behaviour Manager/Specialist','Bursar/Business Manager',
+            'Careers Advisor','Child Protection Liaison Officer','Childcare Officer'
+        ];
+        
         $salaryValues = [25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000];
+        
         for ($i = 0; $i < count($values); $i++) {
             $role = new BusinessRoles();
             $role->setNameOfRole($values[$i]);
             $role->setSalary($salaryValues[$i]);
             $manager->persist($role);
         }
+        
         $manager->flush();
+
     }
 }
--- src/DataFixtures/TitleFixture.php
+++ src/DataFixtures/TitleFixture.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
 use App\Entity\Titles;
@@ -10,11 +12,18 @@ class TitleFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        $values = ['Archbishop','Archdeacon','Baron','Baroness','Bishop','Br','Captain','Cllr','Colonel','Commander','Count','Countess','Dame','Dr','Fr','Lady','Lieutenant','Lieutenant Colonel','Lord','Major','Miss','Mr','Mrs','Ms','Mx','Pr','Prof','Rabbi','Rev','Sergeant','Sir','Sister','The Honorable','Ven'];
+        $values = [
+            'Archbishop','Archdeacon','Baron','Baroness','Bishop','Br','Captain','Cllr','Colonel','Commander',
+            'Count','Countess','Dame','Dr','Fr','Lady','Lieutenant','Lieutenant Colonel','Lord','Major','Miss',
+            'Mr','Mrs','Ms','Mx','Pr','Prof','Rabbi','Rev','Sergeant','Sir','Sister','The Honorable','Ven'
+        ];
+        
         for ($i = 0; $i < count($values); $i++) {
+
             $title = new Titles();
             $title->setTitleName($values[$i]);
             $manager->persist($title);
+
         }
 
         $manager->flush();
