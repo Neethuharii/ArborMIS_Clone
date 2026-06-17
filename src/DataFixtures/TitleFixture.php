a02310052ab8e73313013cc8fa1cdb575e047035
jenitta2 <jenittasupriya@gmail.com>
2026-06-17 14:02:22 +0530

    Added values for title table

---
+++ src/DataFixtures/TitleFixture.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Titles;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TitleFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $values = ['Archbishop','Archdeacon','Baron','Baroness','Bishop','Br','Captain','Cllr','Colonel','Commander','Count','Countess','Dame','Dr','Fr','Lady','Lieutenant','Lieutenant Colonel','Lord','Major','Miss','Mr','Mrs','Ms','Mx','Pr','Prof','Rabbi','Rev','Sergeant','Sir','Sister','The Honorable','Ven'];
+        for ($i = 0; $i < count($values); $i++) {
+            $title = new Titles();
+            $title->setTitleName($values[$i]);
+            $manager->persist($title);
+        }
+
+        $manager->flush();
+    }
+}
