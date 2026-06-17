0ac4b47a2bd9dd953036ccbe88fc14f26db692b3
jenitta2 <jenittasupriya@gmail.com>
2026-06-17 14:47:42 +0530

    Added data to both roles and titles master table

---
+++ src/DataFixtures/RoleFixture.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\BusinessRoles;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class RoleFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $values = ['Administration Assistant','Admission Officer','Advisory Teacher','Art/Design Technician','Assistant Head Teacher','Attendance Officer','Behaviour Manager/Specialist','Bursar/Business Manager','Careers Advisor','Child Protection Liaison Officer','Childcare Officer'];
+        $salaryValues = [25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000, 65000, 70000, 75000];
+        for ($i = 0; $i < count($values); $i++) {
+            $role = new BusinessRoles();
+            $role->setNameOfRole($values[$i]);
+            $role->setSalary($salaryValues[$i]);
+            $manager->persist($role);
+        }
+        $manager->flush();
+    }
+}
