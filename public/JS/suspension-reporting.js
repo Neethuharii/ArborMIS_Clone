54af78ea7937f61abdb6cf093551a8cfe5609195
Mohammed Sahal <mohammed.sahal@integris-education.com>
2026-06-18 15:35:54 +0530

    create a JS folder for storing javascript and formatted the twig file

---
+++ public/JS/suspension-reporting.js
@@ -0,0 +1,18 @@
+const drawer = document.getElementById('drawer');
+const overlay = document.getElementById('overlay');
+
+document.getElementById('openDrawer').addEventListener('click', () => {
+    drawer.classList.add('open');
+    overlay.classList.add('show');
+});
+
+document.getElementById('cancelDrawer').addEventListener('click', () => {
+    drawer.classList.remove('open');
+    overlay.classList.remove('show');
+});
+
+overlay.addEventListener('click', () => {
+    drawer.classList.remove('open');
+    overlay.classList.remove('show');
+});
+
