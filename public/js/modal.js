146cf3bd72c90dedbe7a0e52f597a4a90c1e2f5f
Neethu <Neethu@gmail.com>
2026-06-25 17:58:18 +0530

    added +add icon for adding some features

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     
     var modal = document.getElementById("identityModal");
-  
     var identityClick = document.getElementById("identityAdd"); 
     var span = document.getElementsByClassName("close")[0];
 
@@ -9,10 +8,19 @@ window.addEventListener('DOMContentLoaded', (event) => {
     var contactModal = document.getElementById("contactModal");
     var cspan = document.getElementsByClassName("contactClose")[0];
 
+    var guardianContactClick = document.getElementById("contactAddGuardian");
+    var guardianContactModal = document.getElementById("guardianContactModal");
+    var gspan = document.getElementsByClassName("guardianContactClose")[0];
+
     var qualificationClick = document.getElementById("qualificationAdd"); 
     var qualificationModal = document.getElementById("qualificationModal");
     var qspan = document.getElementsByClassName("qualificationClose")[0];
 
+    if (identityClick && modal) {
+    identityClick.onclick = function() {
+        modal.style.display = "block";
+    };
+}
     identityClick.onclick = function() {
         modal.style.display = "block";
     }
@@ -21,9 +29,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
         contactModal.style.display = "block";
     }
 
+    if (guardianContactClick && guardianContactModal) {
+    guardianContactClick.onclick = function() {
+        guardianContactModal.style.display = "block";
+    }
+}
+
+  if (qualificationClick && qualificationModal) {
     qualificationClick.onclick = function () {
         qualificationModal.style.display = "block";
     }
+}
 
     span.onclick = function() {
         modal.style.display = "none";
@@ -33,16 +49,22 @@ window.addEventListener('DOMContentLoaded', (event) => {
         contactModal.style.display = "none";
     }
 
+     if (gspan && guardianContactModal) {
+    gspan.onclick = function() {
+        guardianContactModal.style.display = "none";
+    }
+}
     qspan.onclick = function() {
         qualificationModal.style.display = "none";
     }
-
 
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }else if (event.target == contactModal) {
             contactModal.style.display = "none";
+        }else if (event.target == guardianContactModal) {
+            guardianContactModal.style.display = "none";
         }else if (event.target == qualificationModal) {
             contactModal.style.display = "none";
         }
