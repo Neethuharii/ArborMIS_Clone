5334d9ee8ea9f36f6a695da7733fd4ef969e0a98
Mohammed Sahal <mohammed.sahal@integris-education.com>
2026-07-03 11:09:57 +0530

    removed unused dependency

--- src/Controller/PointsController.php
+++ src/Controller/PointsController.php
@@ -12,13 +12,13 @@ use App\Repository\StudentPointsRepository;
 
 class PointsController extends AbstractController{
     #[Route('/points/recent', name:'app_points_recent')]
-    public function recent(Request $request):Response
+    public function recent():Response
     {
         return $this->render('Point-awards/recent-points.html.twig');
     }
 
     #[Route('/points/total' , name:'app_points_total')]
-    public function total(Request $request, StudentPointsRepository $studentPointRepo):Response
+    public function total(StudentPointsRepository $studentPointRepo):Response
     {
         return $this->render('Point-awards/total-points.html.twig',[
             'totals'=>$studentPointRepo->findAll()
